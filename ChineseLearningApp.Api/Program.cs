834c2bcecd73e2c7a3dd1e57bfcb8e171cf82d96
Ha Duc Minh <haduc.min507@gmail.com>
2026-08-15 20:51:03 +0700

    Update Program.cs

--- ChineseLearningApp.Api/Program.cs
+++ ChineseLearningApp.Api/Program.cs
@@ -1,14 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
-// Đăng ký DbContext vào hệ thống
 builder.Services.AddDbContext<ChineseLearningApp.Api.Models.ChineseLearningDbContext>();
 
-// Add services to the container.
 
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
 });
-// 1. Cấp quyền CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
@@ -16,13 +13,11 @@ builder.Services.AddCors(options =>
         policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
     });
 });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -31,7 +26,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// 2. Kích hoạt CORS
 app.UseCors("AllowAll");
 
 app.UseAuthorization();
